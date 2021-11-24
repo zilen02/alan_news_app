e00e341e77792d2b4344984f5042999059aa639b
zilen02 <zilenmodi@gmail.com>
2021-11-25 04:23:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import wordsToNumbers from "words-to-numbers";
 
-function App() {
+import NewsCards from "./components/NewsCards/NewsCards";
+import data from "./data";
+import classNames from "classnames";
+import useStyles from "./styles";
+import logo from "./alan_logo.png";
+
+const alankey =
+  "e86066d84b88938a4999c0d0d05bf3e52e956eca572e1d8b807a3e2338fdd0dc/stage";
+
+const App = () => {
+  const classes = useStyles();
+  const [newArticles, setNewArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
+  //   useEffect(() => {
+  //       setNewArticles(data);
+  //   },[])
+  useEffect(() => {
+    alanBtn({
+      key: alankey,
+      onCommand: ({ command, articles ,number }) => {
+        if (command === "newHeadlines") {
+          setNewArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === "highlight") {
+          setActiveArticle((prev) => prev + 1);
+        } else if(command === "open") {
+            console.log(number);
+            const newNumber = number.length > 2 ? wordsToNumbers(number,{fuzzy:true}) : number;
+            const article = articles[newNumber-1];
+            if(newNumber > 20) {
+                alanBtn().playText('Please try that again.');
+            }
+            else {
+                window.open(article.url, "_blank");
+                alanBtn().playText("Openning...");
+            }
+            
+        }
+      },
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <div className={classes.logoContainer}>
+          <img
+            src={logo}
+            className={classes.alanLogo}
+            alt="logo"
+            style={{ backgroundColor: "black" }}
+          />
+        </div>
+        <NewsCards articles={newArticles} activeArticle={activeArticle} />
+      </div>
+    </>
   );
-}
+};
 
 export default App;
+
+//news api
+//1st id
+//dfb4e68a537643838a8c21d175359cc2
+
+//2nd id
+//e53014783a1a4310af3d3e05182bb149
---
+++ src/data.js
@@ -0,0 +1,168 @@
+const data = [
+  {
+    author: "Melanie Zanona, CNN",
+    content:
+      "(CNN)Donald Trump and his allies are starting to dangle the threat of the speakership over House Minority Leader Kevin McCarthy's head, teeing up a yearlong dynamic that is already emboldening the MA… [+8222 chars]",
+    description:
+      "Donald Trump and his allies are starting to dangle the threat of the speakership over House Minority Leader Kevin McCarthy's head, teeing up a yearlong dynamic that is already emboldening the MAGA wing of the GOP conference and influencing the California Repu…",
+    publishedAt: "2021-11-24T17:26:13Z",
+    source: {
+      id: "cnn",
+      name: "CNN",
+    },
+    title:
+      "Trump's influence looms over McCarthy's race to be next House speaker",
+    url: "http://us.cnn.com/2021/11/24/politics/kevin-mccarthy-speakership-race/index.html",
+    urlToImage:
+      "https://cdn.cnn.com/cnnnext/dam/assets/210128150149-03-kevin-mccarthy-trump-file-super-tease.jpg",
+  },
+  {
+    author: "Dalal Mawad, CNN",
+    content:
+      "(CNN)At least 27 migrants are dead after a boat capsized in the English Channel, off the coast of Calais, northern France on Wednesday, police sources told CNN affiliate BFMTV.\r\nFrance's regional mar… [+679 chars]",
+    description:
+      "At least 27 migrants are dead after a boat capsized in the English Channel, off the coast of Calais, northern France on Wednesday, police sources told CNN affiliate BFMTV.",
+    publishedAt: "2021-11-24T17:13:00Z",
+    source: {
+      id: "cnn",
+      name: "CNN",
+    },
+    title:
+      "At least 27 migrants reported dead after boat sinks off French coast",
+    url: "http://www.cnn.com/2021/11/24/europe/france-channel-migrant-deaths-gbr-intl/index.html",
+    urlToImage:
+      "https://cdn.cnn.com/cnnnext/dam/assets/150325082132-social-gfx-breaking-news-super-tease.jpg",
+  },
+  {
+    author: null,
+    content: null,
+    description:
+      "Former President Donald Trump maintains strong support among Republicans even though the party suffered significant losses in both the House and Senate during his presidency. In the latest episode of The Point, CNN's Chris Cillizza talks about how Trump is tr…",
+    publishedAt: "2021-11-24T16:37:29.446062Z",
+    source: {
+      id: "cnn",
+      name: "CNN",
+    },
+    title: "This is Trump's greatest trick - CNN Video",
+    url: "http://us.cnn.com/videos/politics/2021/11/23/trump-greatest-trick-cillizza-the-point.cnn",
+    urlToImage:
+      "https://cdn.cnn.com/cnnnext/dam/assets/211123104914-the-point-trump-trick-super-tease.jpg",
+  },
+  {
+    author: "Paul R. La Monica, CNN Business",
+    content:
+      "New York (CNN Business)Nobody in their right mind likes to pay higher prices for stuff. But if you're an investor, there is a way to profit from inflationary trends and it might be time to start adju… [+2610 chars]",
+    description:
+      "Nobody in their right mind likes to pay higher prices for stuff. But if you're an investor, there is a way to profit from inflationary trends — and it might be time to start adjusting your portfolio accordingly.",
+    publishedAt: "2021-11-24T16:01:22Z",
+    source: {
+      id: "cnn",
+      name: "CNN",
+    },
+    title: "Inflation isn't going away. Here's how to make money from it",
+    url: "http://us.cnn.com/2021/11/24/investing/inflation-stocks-sectors/index.html",
+    urlToImage:
+      "https://cdn.cnn.com/cnnnext/dam/assets/211117114654-restricted-us-inflation-groceries-1111-super-tease.jpg",
+  },
+  {
+    author: "Ariane de Vogue, CNN Supreme Court Reporter",
+    content: null,
+    description:
+      "The Supreme Court said Wednesday that it will hear a case brought by Republican legislators in North Carolina who are seeking to defend the state's strict voter ID law because they think the Democratic state attorney general is not adequately representing the…",
+    publishedAt: "2021-11-24T15:19:10Z",
+    source: {
+      id: "cnn",
+      name: "CNN",
+    },
+    title:
+      "Supreme Court will hear dispute over GOP legislators' defense of North Carolina voter ID law",
+    url: "http://us.cnn.com/2021/11/24/politics/supreme-court-north-carolina-voter-id/index.html",
+    urlToImage:
+      "https://cdn.cnn.com/cnnnext/dam/assets/210917175136-north-carolina-polling-place-file-super-tease.jpg",
+  },
+  {
+    author: "Gabby Orr, CNN",
+    content:
+      "(CNN)A pair of payments the Republican National Committee made to a law firm representing former President Donald Trump is raising questions among former and current GOP officials about the party's p… [+6970 chars]",
+    description:
+      "A pair of payments the Republican National Committee made to a law firm representing former President Donald Trump is raising questions among former and current GOP officials about the party's priorities in a critical election year and its ability to remain n…",
+    publishedAt: "2021-11-24T15:11:29Z",
+    source: {
+      id: "cnn",
+      name: "CNN",
+    },
+    title:
+      "'Nothing about this is normal': RNC payments to Trump attorneys irk GOP officials",
+    url: "http://us.cnn.com/2021/11/24/politics/trump-rnc-payments/index.html",
+    urlToImage:
+      "https://cdn.cnn.com/cnnnext/dam/assets/211117193524-donald-trump-file-100921-super-tease.jpg",
+  },
+  {
+    author: "Analysis by Stephen Collinson, CNN",
+    content: null,
+    description:
+      "When a president has to assure Americans there'll be enough turkey for Thanksgiving, it's a sure sign of national malaise.",
+    publishedAt: "2021-11-24T14:16:33Z",
+    source: {
+      id: "cnn",
+      name: "CNN",
+    },
+    title:
+      "Analysis: Biden spins a message of hope as an anxious nation readies for Thanksgiving",
+    url: "http://us.cnn.com/2021/11/24/politics/joe-biden-anxious-nation-thanksgiving/index.html",
+    urlToImage:
+      "https://cdn.cnn.com/cnnnext/dam/assets/211123150219-02-biden-economy-remarks-super-tease.jpg",
+  },
+  {
+    author:
+      'By Mike Hayes, <a href="/profiles/aditi-sandal">Aditi Sangal</a>, <a href="/profiles/adrienne-vogt">Adrienne Vogt</a>, <a href="/profiles/meg-wagner">Meg Wagner</a> and <a href="/profiles/melissa-mahtani">Melissa Mahtani</a>, CNN',
+    content:
+      'The trial jury consisting of 11 White jurors and one Black juror will soon continue deliberating on the charges facing Travis McMichael, his father Gregory McMichael and their co-defendant William "R… [+1164 chars]',
+    description:
+      "The jury in the trial of three men accused of killing Ahmaud Arbery will continue deliberations today. Follow here for live updates as we wait for a verdict.",
+    publishedAt: "2021-11-24T12:52:55Z",
+    source: {
+      id: "cnn",
+      name: "CNN",
+    },
+    title: "Live updates: Ahmaud Arbery killing trial and verdict watch",
+    url: "https://www.cnn.com/us/live-news/ahmaud-arbery-killing-trial-verdict-watch-11-24-21/h_68921246a3fdb55a7d412406a508101c",
+    urlToImage:
+      "https://cdn.cnn.com/cnnnext/dam/assets/211123100552-08-arbery-trial-11-23-2021-courtroom-super-tease.jpg",
+  },
+  {
+    author: "Brian Rokus, CNN",
+    content:
+      "(CNN)Former President Donald Trump said he was recently visited at his Palm Beach, Florida, resort by Kyle Rittenhouse, the teenager who was acquitted last week on all charges after fatally shooting … [+2179 chars]",
+    description:
+      "Former President Donald Trump said he was recently visited at his Palm Beach, Florida, resort by Kyle Rittenhouse, the teenager who was acquitted last week on all charges after fatally shooting two people and wounding a third during protests in Kenosha, Wisco…",
+    publishedAt: "2021-11-24T04:47:11Z",
+    source: {
+      id: "cnn",
+      name: "CNN",
+    },
+    title: "Trump says Rittenhouse visited him at Mar-a-Lago",
+    url: "http://us.cnn.com/2021/11/23/politics/trump-kyle-rittenhouse-mar-a-lago-visit/index.html",
+    urlToImage:
+      "https://cdn.cnn.com/cnnnext/dam/assets/210527101033-donald-trump-2018-file-super-tease.jpg",
+  },
+  {
+    author: "Opinion by Nicole Hemmer",
+    content:
+      'Nicole Hemmer is an associate research scholar at Columbia University with the Obama Presidency Oral History Project and the author of "Messengers of the Right: Conservative Media and the Transformat… [+5783 chars]',
+    description:
+      "Nicole Hemmer says it's not surprising that Kyle Rittenhouse's first post-acquittal interview was with Tucker Carlson. The right had made Rittenhouse a hero while the bodies of the people he shot were still warm. Carlson is there to tell them that not only wa…",
+    publishedAt: "2021-11-24T00:28:27Z",
+    source: {
+      id: "cnn",
+      name: "CNN",
+    },
+    title:
+      "Opinion: Tucker Carlson just added Kyle Rittenhouse to his pantheon of victim-heroes",
+    url: "http://us.cnn.com/2021/11/23/opinions/tucker-carlson-kyle-rittenhouse-interview-hemmer/index.html",
+    urlToImage:
+      "https://cdn.cnn.com/cnnnext/dam/assets/211122221119-rittenhouse-fox-news-intv-screengrab-vpx-super-tease.jpeg",
+  },
+];
+
+export default data;
